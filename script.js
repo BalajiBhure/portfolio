a9400049c9caa5617630a8fe7a685d4c8b3523ba
BalajiBhure <balajibhure3@gmail.com>
2025-08-14 17:55:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Portfolio website loaded successfully!");
 });
+
+const toggle = document.querySelector('#theme-toggle');
+toggle.addEventListener('click', () => {
+  document.documentElement.dataset.theme =
+    document.documentElement.dataset.theme === 'dark' ? 'light' : 'dark';
+});
